c460b7207158905cd9dc833006e9edb1964edc66
Erdinch <erdinchorhan02@gmail.com>
2022-09-24 00:00:09 +0300

    test

--- src/App.js
+++ src/App.js
@@ -8,20 +8,28 @@ class App extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      personalInfo: {},
+      experience: {},
+      education: {},
+    };
   }
 
   onSubmitForm = (e) => {
     e.preventDefault();
+    console.log(this.state.personalInfo);
+    console.log(this.state.experience);
+    console.log(this.state.education);
   };
 
   render() {
+    const { personalInfo, experience, education } = this.state;
     return (
       <div className="main-container">
         <form onSubmit={this.onSubmitForm} className="forum">
-          <PersonalInfo />
-          <Experience />
-          <Education />
+          <PersonalInfo database={personalInfo} />
+          <Experience database={experience} />
+          <Education database={education} />
         </form>
       </div>
     );
